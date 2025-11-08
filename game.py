fbcc9e2fadc6253462ef66a2d2c4640503cbce20
lubosh <habarta.lubomir@gmail.com>
2025-11-08 15:52:35 +0100

    all

--- game.py
+++ game.py
@@ -1,6 +1,17 @@
 import random
 from random import randint
 import math
+from rich.console import Console
+import curses
+import os
+
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+console = Console()
 class Game:
     def __init__(self, player, location):
         self.player = player
@@ -9,6 +20,10 @@ class Game:
         self.can_flee = True
         self.mode = "EXPLORE"
         self.encounter = None
+        self.stdscr = None
+        self.combat_log = []
+        self.player_log = []
+        self.encounter_log = []
         self.commands = {
             "EXPLORE" : {
                 "M" : self.cmd_move,
@@ -20,18 +35,48 @@ class Game:
                 "F" : self.cmd_flee,
             }
         }
+
+    def start(self):
+        curses.wrapper(self.run)  # <- this is where curses starts!
+
+    def run(self, stdscr):
+        self.stdscr = stdscr
+        curses.curs_set(0)
+        curses.mousemask(0)
+        self.stdscr.keypad(True)
+        self.stdscr.nodelay(False)
+
+        #Enable colors and define them
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_RED, -1)
+        curses.init_pair(2, curses.COLOR_GREEN, -1)
+        curses.init_pair(3, curses.COLOR_CYAN, -1)
+
+        self.main_game_loop()
+
     def main_game_loop(self):
         while True:
+
+
+            self.stdscr.erase()
             if self.mode == "EXPLORE":
                 prompt = "[M] to move, [A] to hunt"
             else:
                 prompt = "[A] to attack, [F] to flee"
-            cmd = input(f"{prompt}\n").strip().upper()
+            self.print_player_info()
+            self.print_combat_log()
+            self.print_encounter_info()
+            self.stdscr.addstr(10, 0, f"{prompt}")
+            self.stdscr.refresh()
+            cmd = self.stdscr.get_wch()
+            self.stdscr.refresh()
             action = self.commands[self.mode][cmd]
             if not action:
                 print("Unknown command. Press ? for help.")
                 continue
             action()
+            self.stdscr.refresh()
 
     def cmd_help(self):
         if(self.mode == "EXPLORE"):
@@ -58,6 +103,11 @@ class Game:
     def cmd_attack(self):
         dmg_amount = self.player.attack()
         self.encounter.take_damage(dmg_amount, self.player)
+        #console.print(f"Hit for  [[red]-{dmg_amount}[/red] HP].")
+        #console.print(f"{self.encounter.name} has [[red]{self.encounter.hp}[/red] HP].")
+
+        log = [("Hit for ", 0), (f"-{dmg_amount}", curses.color_pair(1)), (" HP", 2)]
+        self.log_append(log)
 
     def cmd_flee(self):
         if(self.can_flee):
@@ -74,6 +124,50 @@ class Game:
                 self.can_flee = False
         else:
             pass
+
+    def log_append(self, log):
+        self.combat_log.append(log)
+
+    def print_combat_log(self):
+        self.stdscr.addstr(5, 0, "")
+        for line in self.combat_log[-4:]:
+            for text, attr in line:
+                self.stdscr.addstr(text, attr)
+            self.stdscr.addstr("\n")
+
+    def update_player_log(self):
+        log = [
+            (f"{self.player.name} [", 0),
+            (f"{self.player.hp}", curses.color_pair(1)),
+            (f"] HP", 0)
+        ]
+        self.player_log = log
+
+    def update_encounter_info(self):
+        if(self.encounter != None):
+            log = [
+                (f"{self.encounter.name} [", 0),
+                (f"{self.encounter.hp}", curses.color_pair(1)),
+                (f"] HP", 0)
+            ]
+            self.encounter_log = log
+
+    def print_encounter_info(self):
+        self.update_encounter_info()
+        self.stdscr.addstr(0, 18, "")
+
+        for text, attr in self.encounter_log:
+            self.stdscr.addstr(text, attr)
+
+    def print_player_info(self):
+        self.update_player_log()
+        self.stdscr.addstr(0, 0, "")
+
+        for text, attr in self.player_log:
+            self.stdscr.addstr(text, attr)
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from player import Character
 from location import Location
+from rich.console import Console
 from game import Game
+import curses
+console = Console()
+
+
 NPC1 = Character("Greg", 5, 11)
 NPC2 = Character("Greg", 10, 22)
 NPC3 = Character("Greg", 15, 33)
 NPC4 = Character("Greg", 20, 44)
 NPC5 = Character("Bear", 20, 8)
-Player = Character("Luba", 2, 2)
+Player = Character("Luba", 7, 2)
 enemies = [NPC1, NPC2, NPC3, NPC4]
+
+console.print("[bold green]Welcome hero![/]")
 
 dark_forest = Location("Dark Forest", "Bear")
 dark_forest.add_enemy(NPC5)
@@ -16,20 +23,10 @@ dark_forest.print_info()
 NPC4.level = 1
 
 Player.level = 1
+
+
 Game = Game(Player, dark_forest)
-Game.main_game_loop()
-'''
-while(True):
-    decision = input("Press 'A' to attack\n")
-    NPC = None
-    for NPC in enemies:
-        if(NPC.alive == True):
-            break
-    if(decision == 'A'):
-        damage = Player.attack()
-        NPC.take_damage(damage, Player)
-        print("Greg takes -" + str(damage) + " dmg")
-        print("Greg hp[" + str(NPC.show_hp()) + "]")
-        print("Player xp[" + str(Player.show_xp()) + "]\n")
-'''
+#Game.start()
+Game.start()
+
 
